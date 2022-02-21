5d45dac405a0984c2e1a152921be8bac4b2d9e69
swathi23897 <63533884+swathi23897@users.noreply.github.com>
2022-02-21 12:47:33 +0530

    Update is-auth

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
         req.isAuth = false
         return next();
     }
-    const token = authHeader.split(' ')[1];  //Authorization: Bearer fsdskliedgkjkdm
+    const token = authHeader.split(' ')[1];  //Authorization: Bearer token 
     if (!token || token === '') {
 
         req.isAuth = false
